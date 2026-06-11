00ddb2485a642185dd3dd1543e3bad137bc4a847
SzymonKonikiewicz <szymon.konikiewicz@gmail.com>
2026-06-11 15:22:28 +0200

    Added basic style concept, implemented forms

--- app.py
+++ app.py
@@ -54,8 +54,6 @@ st.write("Tutaj będzie krótki opis naszego projektu")
 
 st.divider()
 
-st.header("Proszę wpisać dane: ")
-
 user_data = {}
 
 # New data frame without a price column
@@ -72,29 +70,36 @@ with st.sidebar:
         index=0,
     )
 
-# Panel for user input
-columns = st.columns(4)
+st.header("Parametry mieszkania")
+with st.form("user_input_form", border=True):
+        # Panel for user input
+    columns = st.columns(4)
 
-for index, c in enumerate(column_names):
-    actual = columns[index % 4]
 
-    unique_values = new_df[c].dropna().unique()
 
-    # mean = float(new_df[c].mean())
+    for index, c in enumerate(column_names):
+        actual = columns[index % 4]
 
-    with actual:
-        if new_df[c].dtype in ["object", "string"] or len(unique_values) <= 2:
-            options = unique_values.tolist()
+        unique_values = new_df[c].dropna().unique()
 
-            user_data[c] = st.selectbox(label=f"{c}", options=options)
-        else:
-            user_data[c] = st.number_input(label=f"{c}", value=0.0, step=None)
+        # mean = float(new_df[c].mean())
 
-st.divider()
+        with actual:
+            if new_df[c].dtype in ["object", "string"] or len(unique_values) <= 2:
+                options = unique_values.tolist()
+
+                user_data[c] = st.selectbox(label=f"{c}", options=options)
+            else:
+                user_data[c] = st.number_input(label=f"{c}", value=0.0, step=None)
+
+    st.divider()
+
+    submitted = st.form_submit_button("Wygeneruj predykcję ceny! 😜", type="primary", use_container_width=True)
+
 
 # test button
 
-if st.button("Przewidywanie ceny", type="primary", use_container_width=True):
+if submitted:
     input_df = pd.DataFrame([user_data])
 
     st.write("Podgląd inputu użytkownika")
